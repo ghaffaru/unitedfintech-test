3a4a4d0ac0cec804671107c63069fc8e0093f1e9
Ghaffaru <mudashiruagm@gmail.com>
2020-10-03 07:43:37 +0000

    fix ci build

--- unitedfintech_test/settings.py
+++ unitedfintech_test/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +90,7 @@ DATABASES = {
         'ENGINE': 'djongo',
         'NAME': 'employees',
         'CLIENT': {
-           'host': 'db',
+           'host': env("DB_HOST", default="127.0.0.1"),
         }
     }
 }
